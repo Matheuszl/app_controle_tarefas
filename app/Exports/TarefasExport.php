dddfe6fd52117a95e6c9e2214ca41638b1de1fcb
Matheuszl <matheus.zzalamena@gmail.com>
2022-01-24 20:30:38 -0300

    cabeçalho das exportaçoes

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,13 +4,14 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 /**
  * ESSE DIRETORIO É CRIADO 
  * php artisan make:export UsersExport --model=User
  * PARA EXPORTAÇÃO DE ARQUIVOS XLSX
  */
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -20,5 +21,12 @@ class TarefasExport implements FromCollection
         //return Tarefa::all(); TRAS TODOS OS REGISTROS DE TAREFA MAPEADO POR ELE MESMO
         // dd(auth()->user()->tarefas()->get());
         return auth()->user()->tarefas()->get(); //exposta apenas as tarefas relacionadas ao usuario logado
+    }
+
+    //a interface espera que o metodo retorne um array
+    // :array declaramos o tipo do retorno
+    public function headings():array 
+    {   
+        return ['ID Tarefa', 'ID Usuario', 'Tarefa', 'Data Conclusão', 'Data Criação', 'Data Atualização']; 
     }
 }

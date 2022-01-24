8e23777a747c6889313ca62bd474997314caab78
Matheuszl <matheus.zzalamena@gmail.com>
2022-01-24 18:47:53 -0300

    navegaçao superior para tarefas

--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -5,7 +5,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Tarefas</div>
+                    <div class="card-header">Tarefas
+                        <a href="{{ route('tarefa.create') }}" class="float-end">Nova Tarefa</a>
+                    </div>
+
 
                     <div class="card-body">
 
@@ -27,11 +30,13 @@
                                         <td>{{ date('d/m/Y', strtotime($t['data_conclusao'])) }}</td>
                                         <td><a href="{{ route('tarefa.edit', $t['id']) }}">Editar</a></td>
                                         <td>
-                                            <form id="form_{{$t['id']}}" method="POST" action="{{ route('tarefa.destroy', ['tarefa' => $t['id']]) }}">
+                                            <form id="form_{{ $t['id'] }}" method="POST"
+                                                action="{{ route('tarefa.destroy', ['tarefa' => $t['id']]) }}">
                                                 @method('DELETE')
                                                 @csrf
-                                                <a href="#" onclick="document.getElementById('form_{{$t['id']}}').submit()">Excluir</a>
-                                            </form> 
+                                                <a href="#"
+                                                    onclick="document.getElementById('form_{{ $t['id'] }}').submit()">Excluir</a>
+                                            </form>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -45,7 +50,8 @@
                                         href="{{ $tarefas->previousPageUrl() }}">Voltar</a></li>
 
                                 @for ($i = 1; $i <= $tarefas->lastPage(); $i++)
-                                    <li class="page-item {{ $tarefas->currentPage() == $i ? 'active' : ''}}"><a class="page-link"
+                                    <li class="page-item {{ $tarefas->currentPage() == $i ? 'active' : '' }}"><a
+                                            class="page-link"
                                             href="{{ $tarefas->url($i) }}">{{ $i }}</a></li>
                                 @endfor
 

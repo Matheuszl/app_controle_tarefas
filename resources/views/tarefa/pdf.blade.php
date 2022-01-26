d704e8fe9db77958327582d2e45c7c7a69c25ac4
Matheuszl <matheus.zzalamena@gmail.com>
2022-01-26 17:13:48 -0300

    estilo para o pdf

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,23 +1,60 @@
-<h2>Lista de Tarefas</h2>
+<!DOCTYPE html>
+<html lang="en">
 
-<table>
-  <thead>
-    <tr>
-      <th>ID:</th>
-      <th>Tarefa</th>
-      <th>Data conclusao:</th>
-    </tr>
-  </thead>
-  <body>
-    @foreach ($tarefas as $kay=> $tarefa )
-      <tr>
-        <td>{{ $tarefa->id}}</td>
-        <td>{{ $tarefa->tarefa }}</td>
-        <td>{{ date('d/m/Y', strtotime( $tarefa->data_conclusao) )}}</td>
-      </tr>
-    @endforeach
-    
-  </body>
-</table>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
+    <style>
+        .titulo {
+            border: 1px;
+            background-color: #c2c2c2;
+            text-align: center;
+            width: 100%;
+            text-transform: uppercase;
+            font-weight: bold;
 
+            margin-bottom: 25px;
+
+        }
+
+        .tabela {
+            width: 100%;
+        }
+
+        table th {
+            text-align: left;
+        }
+
+        .page-break {
+            page-break-after: always;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="titulo">Lista de Tarefas</div>
+
+    <table class="tabela">
+        <thead>
+            <tr>
+                <th>ID:</th>
+                <th>Tarefa</th>
+                <th>Data conclusao:</th>
+            </tr>
+        </thead>
+
+        <body>
+            @foreach ($tarefas as $kay => $tarefa)
+                <tr>
+                    <td>{{ $tarefa->id }}</td>
+                    <td>{{ $tarefa->tarefa }}</td>
+                    <td>{{ date('d/m/Y', strtotime($tarefa->data_conclusao)) }}</td>
+                </tr>
+            @endforeach
+
+        </body>
+    </table>
+</body>
+
+</html>
